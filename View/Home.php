24125cb5999bb08cf609db90486088d022ae8f2b
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-17 18:27:04 -0300

    Cria a sessão, coloca a pokebola girando

---
+++ View/Home.php
@@ -0,0 +1,85 @@
+<?php
+if (isset($_SESSION['usuario_fez_login'])) {
+?>
+    <h1>Bem vindo, <?php echo $_SESSION['nome_usuario'] ?></h1>
+<?php } else { ?>
+    <h1>Bem vindo à Clínica Umbrella!</h1>
+<?php } ?>
+<!----------------------------------------------------------------------------------------------------------------------------->
+<hr>
+<div class="grade">
+    <img id="img-home" src="img/pokeball.png" alt="Umbrella">
+    <!---------------------------------------------------------------------------------------------------------------------------->
+    <ul class="slider">
+        <li>
+            <input type="radio" id="slide1" name="slide" checked>
+            <label for="slide1"></label>
+            <img id="img-slide" src="Img/Carvalho.png" alt="Professor Carvalho" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide2" name="slide" checked>
+            <label for="slide2"></label>
+            <img id="img-slide" src="Img/Professor_Elm.png" alt="Professor Elm" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide3" name="slide" checked>
+            <label for="slide3"></label>
+            <img id="img-slide" src="Img/Professor_Birch.png" alt="Professor Birch" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide4" name="slide" checked>
+            <label for="slide4"></label>
+            <img id="img-slide" src="Img/Professor_Rowan.png" alt="Professor Birch" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide5" name="slide" checked>
+            <label for="slide5"></label>
+            <img id="img-slide" src="Img/Professora_Juniper.png" alt="Professor Birch" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide6" name="slide" checked>
+            <label for="slide6"></label>
+            <img id="img-slide" src="Img/Professor_Sycamore.png" alt="Professor Birch" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide7" name="slide" checked>
+            <label for="slide7"></label>
+            <img id="img-slide" src="Img/Professor_Kukui.png" alt="Professor Birch" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide8" name="slide" checked>
+            <label for="slide8"></label>
+            <img id="img-slide" src="Img/Professor_Magnolia.png" alt="Professor Birch" />
+        </li>
+        <!------------------------------------------------------------------------------------------------------------------>
+        <li>
+            <input type="radio" id="slide9" name="slide" checked>
+            <label for="slide9"></label>
+            <img id="img-slide" src="Img/Professora_Sonia.png" alt="Professor Birch" />
+        </li>
+    </ul>
+</div>
+<!---------------------------------------------------------------------------------------------------------------------------->
+<script>
+    let deg = 0;
+    let img_home = document.getElementById("img-home")
+
+    // Faz o javascript esperar por uma certa quantidade de milisegundos
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    (async function girarImagem() {
+        img_home.style.transform = `rotate(${deg}deg)`
+        deg = deg >= 360 ? 0 : deg + 1
+        await sleep(10)
+        girarImagem()
+    })()
+</script>
