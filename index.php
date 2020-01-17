c2b81b0c0b2e80a764426c9d00dc34431dc6ed03
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-17 18:28:42 -0300

    separa e chama as paginas

--- index.php
+++ index.php
@@ -1,46 +1,25 @@
-<?php include_once 'Header.php'; ?>
+<?php 
+session_start();
+include_once 'Header.php'; ?>
 
 <div class="body-container">
 
-<ul class="slider">
-   <li>
-       <input type="radio" id="slide1" name="slide" checked>
-        <label for="slide1"></label>
-       <img  id="img-slide"  src="Img/Professor_Carvalho.png" alt="Professor Carvalho"/>
-    </li>
-<!------------------------------------------------------------------------------------------------------------------>
-   <li>
-       <input type="radio" id="slide2" name="slide" checked>
-       <label for="slide2"></label>
-       <img  id="img-slide"  src="Img/Professor_Elm.png" alt="Professor Elm"/>
-    </li>
-<!------------------------------------------------------------------------------------------------------------------>   
-   <li>
-       <input type="radio" id="slide3" name="slide" checked>  
-       <label for="slide3"></label>
-       <img  id="img-slide"  src="Img/Professor_Birch.png" alt="Professor Birch"/>
-    </li>
-<!------------------------------------------------------------------------------------------------------------------> 
+<?php
 
+    $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 'home';
 
-
-   <!--
-   <li>Professor Rowan<img  id="img-slide"  src="Img/Professor_Rowan.png" alt="Professor Rowan"/></li>
-
-   <li>Professora Juniper<img  id="img-slide"  src="Img/Professora_Juniper.png" alt="Professora Juniper"/></li>
-
-   <li>Professor Sycamore<img  id="img-slide"  src="Img/Professor_Sycamore.png" alt="Professor Sycamore"/></li>
-
-   <li>Professor Kukui<img  id="img-slide"  src="Img/Professor_Kukui.png" alt="Professor Kukui"/></li>
-
-   <li>Professor Magnolia<img  id="img-slide"  src="Img/Professor_Magnolia.png" alt="Professor Magnolia"/></li>
-
-   <li>Professora Sónia<img  id="img-slide"  src="Img/Professora_Sonia.png" alt="Professora Sónia"/></li>
--->
-</ul>
-
+    switch ($pagina) {
+        case 'login':
+            include_once 'view/Login.php';
+            break;
+        case 'home':
+            include_once 'view/Home.php';
+            break;
+        default:
+            include_once 'view/Home.php';
+    }
+    ?>
 
 </div>
-
 
 <?php include_once 'Footer.php'; ?>
