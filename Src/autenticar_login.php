5a1f45e448a5d2ba0124f5712c09e9a1df80f487
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-17 18:25:35 -0300

    Criar o autenticar login

---
+++ Src/autenticar_login.php
@@ -0,0 +1,26 @@
+<?php
+include "conexao_banco.php";
+$Login = isset($_POST['login']) ? $_POST['login'] : "";
+$Senha = isset($_POST['senha']) ? md5($_POST['senha']) : "";
+
+$sql = "SELECT * FROM `atendente` 
+        WHERE Login = '$Login' AND Senha = '$Senha';";
+
+$result = mysqli_query($con, $sql);
+//prepara o ambiente:inicia a sessão e formata os dados do usuário
+if (mysqli_num_rows($result) == 1) {
+    session_start();
+    // transfrma $result num array associativo
+    $usuario = mysqli_fetch_assoc($result);
+
+    //salva as informações do usuário
+    $_SESSION['usuario_fez_login'] = true;
+    $_SESSION['nome_usuario'] = $usuario['Nome'];
+
+
+    //Fecha a conexão com o banco e recirecionar
+    mysqli_close($con);
+    header('location: http://localhost/Pokemon_clinical/index.php?pagina=Home');
+} else {
+    header('location: http://localhost/Pokemon_clinical/index.php?pagina=login&erro=Login ou senha invalidos');
+}
