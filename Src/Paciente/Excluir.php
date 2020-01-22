ca8efe5b6d4d704323f967e83a886515ed5e04c5
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-22 17:46:54 -0300

    Exclui o paciente

---
+++ Src/Paciente/Excluir.php
@@ -0,0 +1,15 @@
+<?php
+
+//conexão com o banco
+require 'Src/conexao_banco.php';
+
+//pagar id
+$PacienteID = isset($_GET['ID']) ? $_GET['ID'] : 0;
+
+
+//tentar excluir do banco de dados
+$sql = "DELETE FROM paciente WHERE ID = $PacienteID;";
+mysqli_query($con, $sql);
+
+//retornar para a página pacientes
+header('location: http://localhost/Pokemon_clinical/index.php?pagina=Pacientes');
