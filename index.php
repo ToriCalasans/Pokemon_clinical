9a50b746f396253cb869f30d91c2a26e4d566862
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-22 17:51:49 -0300

    add os diretorios

--- index.php
+++ index.php
@@ -10,18 +10,24 @@ include_once 'Header.php'; ?>
 
     switch ($pagina) {
         case 'Login':
-            include_once 'view/Login.php';
+            include_once 'View/Login.php';
             break;
         case 'Home':
-           include_once 'view/Home.php';
-          /* include_once 'view/teste.php';*/
+            include_once 'View/Home.php';
+            /* include_once 'view/teste.php';*/
+            break;
+        case 'Pacientes':
+            include_once 'View/Paciente.php';
             break;
         case 'CadastroAtendente':
-            include_once 'view/CadastroAtendente.php';
+            include_once 'View/Atendente.php';
+            break;
+        case 'SalvarPaciente':
+            include_once 'View/SalvarPaciente.php';
             break;
         default:
-           include_once 'view/Home.php';
-           /* include_once 'view/teste.php';*/
+            include_once 'View/Home.php';
+            /* include_once 'view/teste.php';*/
     }
     ?>
 
