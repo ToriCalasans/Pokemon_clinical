746ea7245ecf04ee6b55e85971faf570adb46eee
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-22 17:50:56 -0300

    Add o jquery, bootstrap e datatables e a tabela de pacientes

--- Footer.php
+++ Footer.php
@@ -1,5 +1,16 @@
 <footer>
-Copyrigth &copy; 2020 - Pokemon corporations
+    Copyrigth &copy; 2020 - Pokemon corporations
+    <script src="JS/Jquery.min.js"></script>
+    <script src="JS/bootstrap.min.js"></script>
+    <script src="JS/datatables.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#Tabela_Pacientes').DataTable();
+        });
+    </script>
+
+
 </footer>
 </body>
 
