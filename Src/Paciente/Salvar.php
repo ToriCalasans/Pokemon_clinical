0a4f66c8096ee168dcf0d30867b7e2e3137496a0
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-22 17:47:28 -0300

    Cadastra ou edita o paciente

---
+++ Src/Paciente/Salvar.php
@@ -0,0 +1,39 @@
+<?php
+
+//conexão com o banco
+require 'Src/conexao_banco.php';
+
+//Receber dados do POST
+$ID = isset($_POST['ID']) ? $_POST['ID'] : 'null';
+$Nome = isset($_POST['Nome']) ? $_POST['Nome'] : '';
+$Nascimento = isset($_POST['Nascimento']) ? $_POST['Nascimento'] : '';
+$CPF = isset($_POST['CPF']) ? $_POST['CPF'] : '';
+
+// == <- verifica o valor ; valor  === <- verifica valor e tipo
+
+//validar os dados
+if (!empty($Nome) && !empty($Nascimento) && !empty($CPF)) {
+
+    //Se o id foi enviado
+    if ($ID !== null) {
+    
+        //atualizar
+        $sql = "UPDATE paciente SET NOME = '$Nome', Nascimento = $Nascimento, CPF = '$CPF' WHERE ID = $ID;";
+
+    // se nao
+    } else {
+    
+        //inserir
+        $sql = "INSERT INTO paciente(Nome, Nascimento, CPF) VALUES ('$Nome','$Nascimento','$CPF');";
+    }
+
+    //Faz a query
+    mysqli_query($con, $sql);
+    header('location: http://localhost/Pokemon_clinical/index.php?pagina=Pacientes');
+    // se não
+
+} else {
+
+    //retornar para a página pacientes
+    header('location: http://localhost/Pokemon_clinical/index.php?pagina=Cadastrar_Paciente&erro=Dados Invalidos'); 
+}
