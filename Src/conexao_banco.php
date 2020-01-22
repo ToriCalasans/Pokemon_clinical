46186354af775c901942c27d0bd0dd5504a15217
Tori <you@example.com>
2020-01-22 01:14:40 -0300

    mudei a senha, lembrar de mudar no ifs

--- Src/conexao_banco.php
+++ Src/conexao_banco.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost','root','aluno','clinica');
+$con = mysqli_connect('localhost','root','','clinica');
 if (mysqli_connect_errno($con)) {
     die (mysqli_error($con));
  }
