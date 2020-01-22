ce09b4c5499af9d3b4e6273326e15af6fbe0867f
Vitoria R. S. Carmo <07599756558@ifslab.edu.br>
2020-01-22 17:47:50 -0300

    mudei a senha, lembrar de mudar em ksa

--- Src/conexao_banco.php
+++ Src/conexao_banco.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost','root','','clinica');
+$con = mysqli_connect('localhost','root','aluno','clinica');
 if (mysqli_connect_errno($con)) {
     die (mysqli_error($con));
  }
